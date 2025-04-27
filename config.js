daa5e0185b07515169f53b72bd5c6e4fbc0f6d0d
sofrenoob <sofrenoob@gmail.com>
2025-04-27 01:46:04 -0300

    Atualizar o config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 config = {
-  dono:"552197721573", //seu número com 55+ddd sem o 9
-  tempo_teste:"3", //tempo do teste em horas
-  valorLogin: 0.10, //valor do login SSH
-  nomeLoja:"Loja teste", //nome da sua loja virtual
-  linkApp:"LinkAqui", //Link do seu app, playStore/Mediafire
-  token_mp:"TokenAqui" //seu token do mercado pago
+  dono:"5521997721573", //seu número com 55+ddd sem o 9
+  tempo_teste:"1", //tempo do teste em horas
+  valorLogin: 10.00, //valor do login SSH
+  nomeLoja:"ALFA INTERNET", //nome da sua loja virtual
+  linkApp:"https://www.dropbox.com/scl/fi/u9alaonuerpuv7ouuxemb/Alfa-Internet-pro.apk?rlkey=fq2w7g3lu2j9pv9w8jijta3wz&st=98nzpe0a&dl=1", //Link do seu app, playStore/Mediafire
+  token_mp:"APP_USR-153110256233483-042622-5e61d8c4f515a3e4858a188558b7b725-1697276618" //seu token do mercado pago
 }
 module.exports = {
   config
