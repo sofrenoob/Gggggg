d0b673e571558a382993863a735db1c1345e62df
sofrenoob <sofrenoob@gmail.com>
2025-04-30 05:11:31 -0300

    Criar s/install.sh

---
+++ s/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Atualizar o sistema e instalar dependências
+echo "Atualizando o sistema e instalando dependências..."
+sudo apt update && sudo apt upgrade -y
+
+# Instalar Python 3, pip e pacotes necessários
+echo "Instalando o Python 3 e pacotes necessários..."
+sudo apt install python3 python3-pip python3-psutil -y
+
+# Instalar ufw (se não estiver instalado)
+echo "Instalando UFW (Firewall)"
+sudo apt install ufw -y
+
+# Configurar permissões para firewall
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+
+# Ativar o firewall
+echo "Ativando firewall..."
+sudo ufw enable
+
+# Dando permissão de execução ao script
+chmod +x scripts/script.py
+
+echo "Instalação completa!"
+echo "Execute o script Python com o comando: python3 scripts/script.py"
