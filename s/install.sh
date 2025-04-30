1dc9adb98bf25a9fb5ed9786f7b1e0541597f2df
sofrenoob <sofrenoob@gmail.com>
2025-04-30 05:53:09 -0300

    Atualizar o install.sh

--- s/install.sh
+++ s/install.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Atualizar o sistema e instalar dependências
-echo "Atualizando o sistema e instalando dependências..."
-sudo apt update && sudo apt upgrade -y
-
-# Instalar Python 3, pip e pacotes necessários
-echo "Instalando o Python 3 e pacotes necessários..."
-sudo apt install python3 python3-pip python3-psutil -y
-
-# Instalar ufw (se não estiver instalado)
-echo "Instalando UFW (Firewall)"
-sudo apt install ufw -y
-
 # Configurar permissões para firewall
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
