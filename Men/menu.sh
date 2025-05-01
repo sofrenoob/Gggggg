3f09ba57f9a6a71d51f1e159103f8f3eec443b44
sofrenoob <sofrenoob@gmail.com>
2025-04-30 23:48:14 -0300

    Atualizar o menu.sh

--- Men/menu.sh
+++ Men/menu.sh
@@ -1,83 +1,42 @@
 #!/bin/bash
-
-# Função para exibir o menu
-exibir_menu() {
-    echo "Escolha uma opção:"
-    echo "1) Opção 1"
-    echo "2) Opção 2"
-    echo "3) Opção 3"
-    echo "4) Opção 4"
-    echo "5) Opção 5"
-    echo "6) Opção 6"
-    echo "7) Opção 7"
-    echo "8) Opção 8"
-    echo "9) Opção 9"
-    echo "10) Opção 10"
-    echo "0) Sair"
+    clear
+    echo -e "\033[01;31m║\033[1;31m\033[5;34;47m                    MENU BADVPN                    \033[1;33m \033[0m\033[01;31m║"
+    echo -e "\033[01;31m║\033[0m"
+    echo -e "\033[01;31m║\033[1;31m\033[1;34m[\033[1;37m 01 •\033[1;34m]\033[1;37m ➩  \033[1;33mPROXY $stsu\033[0m"
+    echo -e "\033[01;31m║\033[1;31m\033[1;34m[\033[1;37m 02 •\033[1;34m]\033[1;37m ➩  \033[1;33mSSLPROXY 2 \033[0m"
+    echo -e "\033[01;31m║\033[1;31m\033[1;34m[\033[1;37m 03 •\033[1;34m]\033[1;37m ➩  \033[1;33mBADVPN 3 \033[0m"
+    echo -e "\033[01;31m║\033[1;31m\033[1;34m[\033[1;37m 04 •\033[1;34m]\033[1;37m ➩  \033[1;33mBADVPN 4 ACKHTTP \033[0m"
+    echo -e "\033[01;31m║\033[1;31m\033[1;34m[\033[1;37m 00 •\033[1;34m]\033[1;37m ➩  \033[1;33mVOLTAR  \033[1;32m<\033[1;33m<\033[1;31m< \033[0m"
+    echo -e "\033[01;31m║\033[0m"
+    echo -e "\033[0;31m╠━━═━═━═━═━═━═━═━═━━═━═━═━═━═━━═━═━═━═━═━━═━═━═━═━═━\033[0m"
+			tput civis
+			echo -ne "\033[1;31m╰━━━━━━━━❪\033[1;32mESCOLHA OPÇÃO DESEJADA\033[1;33m\033[1;31m\033[1;37m : ";
+			read x
+			tput cnorm
+			clear
+			case $x in
+			1 | 01)
+			proxy
+			;;
+			2 | 02)
+			sslproxy
+			;;
+			3 | 03)
+			badpro1
+			;;
+			4 | 04)
+			badvpn3
+			;;
+			0 | 00)
+			clear
+			menu
+			;;
+			*)
+			echo -e "\033[1;31mOpcao invalida !\033[0m"
+			sleep 2
+			;;
+			esac
+		done
+	}
+	fun_conexao
 }
-
-# Função para garantir permissão de execução
-dar_permissao_execucao() {
-    if [ -f "$1" ]; then
-        chmod +x "$1"
-        echo "Permissões de execução atribuídas a $1"
-    else
-        echo "Arquivo $1 não encontrado!"
-    fi
-}
-
-# Loop até o usuário escolher a opção para sair
-while true; do
-    exibir_menu
-    read -p "Digite sua opção (1-10 ou 0 para sair): " opcao
-
-    case $opcao in
-        1)
-            dar_permissao_execucao "./resposta_1.sh"
-            ./resposta_1.sh
-            ;;
-        2)
-            dar_permissao_execucao "./resposta_2.sh"
-            ./resposta_2.sh
-            ;;
-        3)
-            dar_permissao_execucao "./resposta_3.sh"
-            ./resposta_3.sh
-            ;;
-        4)
-            dar_permissao_execucao "./resposta_4.sh"
-            ./resposta_4.sh
-            ;;
-        5)
-            dar_permissao_execucao "./resposta_5.sh"
-            ./resposta_5.sh
-            ;;
-        6)
-            dar_permissao_execucao "./resposta_6.sh"
-            ./resposta_6.sh
-            ;;
-        7)
-            dar_permissao_execucao "./resposta_7.sh"
-            ./resposta_7.sh
-            ;;
-        8)
-            dar_permissao_execucao "./resposta_8.sh"
-            ./resposta_8.sh
-            ;;
-        9)
-            dar_permissao_execucao "./resposta_9.sh"
-            ./resposta_9.sh
-            ;;
-        10)
-            dar_permissao_execucao "./resposta_10.sh"
-            ./resposta_10.sh
-            ;;
-        0)
-            echo "Saindo..."
-            break
-            ;;
-        *)
-            echo "Opção inválida. Tente novamente."
-            ;;
-    esac
-done
