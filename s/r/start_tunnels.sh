328d6dbe1f49184787b28427705f00c14d3b0357
sofrenoob <sofrenoob@gmail.com>
2025-05-03 16:48:47 -0300

    Atualizar o start_tunnels.sh

--- s/r/start_tunnels.sh
+++ s/r/start_tunnels.sh
@@ -8,15 +8,15 @@ killall socat 2>/dev/null
 echo -e "\e[1;32m[+] Iniciando túneis de operadoras...\e[0m"
 
 # Vivo - 7001
-screen -dmS vivo socat TCP4-LISTEN:7001,reuseaddr,fork TCP4:127.0.0.1:1194
+screen -dmS vivo socat TCP4-LISTEN:80,reuseaddr,fork TCP4:127.0.0.1:1194
 echo -e "\e[1;36m[+] Vivo túnel ativo na porta 7001\e[0m"
 
 # Claro - 7002
-screen -dmS claro socat TCP4-LISTEN:7002,reuseaddr,fork TCP4:127.0.0.1:1195
+screen -dmS claro socat TCP4-LISTEN:80,reuseaddr,fork TCP4:127.0.0.1:1195
 echo -e "\e[1;36m[+] Claro túnel ativo na porta 7002\e[0m"
 
 # Tim - 7003
-screen -dmS tim socat TCP4-LISTEN:7003,reuseaddr,fork TCP4:127.0.0.1:1196
+screen -dmS tim socat TCP4-LISTEN:80,reuseaddr,fork TCP4:127.0.0.1:1196
 echo -e "\e[1;36m[+] Tim túnel ativo na porta 7003\e[0m"
 
 # BadVPN - 7300
