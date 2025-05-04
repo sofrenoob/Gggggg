80b59dddb8261652fc13b17474b722da502b481d
sofrenoob <sofrenoob@gmail.com>
2025-05-04 03:23:03 -0300

    Atualizar o install.sh

--- s/r/o/install.sh
+++ s/r/o/install.sh
@@ -1,45 +1,14 @@
 #!/bin/bash
-echo -e "\e[44m\e[97m       ALFA PROXY - INSTALADOR        \e[0m"
-sleep 2
 
-DEST="/opt/alfaproxy"
-sudo mkdir -p $DEST/logs
+echo "[INFO] Atualizando pacotes e instalando dependências..."
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y python3 python3-pip
 
-# Instalar dependências
-apt update && apt install -y python3 python3-pip screen openssl
-pip3 install -r $DEST/requirements.txt
-sudo apt update
-sudo apt install -y python3 python3-pip openssl screen
+echo "[INFO] Baixando o painel..."
+curl -o panel.py https://raw.githubusercontent.com/master-rf-root/seu-repo/main/panel.py
 
-# Gerar certificado SSL
-openssl req -new -x509 -days 365 -nodes -out $DEST/cert.pem -keyout $DEST/key.pem -subj "/CN=AlfaProxy"
+echo "[INFO] Dando permissão ao painel..."
+chmod +x panel.py
 
-# Ativação inicial em background
-screen -dmS alfaproxy python3 $DEST/proxy_server.py
-
-# Instalando libs Python
-pip3 install websocket-server
-
-# Baixando arquivos do repositório
-wget -O $DEST/proxy_server.py https://raw.githubusercontent.com/sofrenoob/Gggggg/main/s/r/o/proxy_server.py
-wget -O $DEST/payloads.txt https://raw.githubusercontent.com/sofrenoob/Gggggg/main/s/r/o/payloads.txt
-wget -O $DEST/cert.pem https://raw.githubusercontent.com/sofrenoob/Gggggg/main/s/r/o/cert.pem
-wget -O $DEST/key.pem https://raw.githubusercontent.com/sofrenoob/Gggggg/main/s/r/o/key.pem
-wget -O $DEST/menuproxy.sh https://raw.githubusercontent.com/sofrenoob/Gggggg/main/s/r/o/menuproxy.sh
-wget -O $DEST/config.txt https://raw.githubusercontent.com/sofrenoob/Gggggg/main/s/r/o/config.txt
-wget -O $DEST/requirements.txt https://raw.githubusercontent.com/sofrenoob/Gggggg/main/s/r/o/requirements.txt
-
-# Configuração padrão de porta
-echo "80" > $DEST/config.txt
-
-# Permissões
-chmod +x $DEST/proxy_server.py
-chmod 600 $DEST/*.pem
-chmod +x $DEST/menuproxy.sh
-
-# Criando alias de menu
-echo 'alias menuproxy="screen -dmS alfa-proxy python3 /opt/alfaproxy/proxy_server.py && echo -e \"\e[44m\e[97m ALFA PROXY RODANDO \e[0m\""' >> ~/.bashrc
-source ~/.bashrc
-
-# Finalizando
-echo -e "\e[42m\e[97m Instalação concluída. Digite 'menuproxy' para iniciar. \e[0m"
+echo "[INFO] Instalação concluída. Execute o painel com:"
+echo "python3 panel.py"
