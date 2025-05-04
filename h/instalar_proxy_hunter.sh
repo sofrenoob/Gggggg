4772cbdd8c823dd7d73cb2d627785bd191278d50
sofrenoob <sofrenoob@gmail.com>
2025-05-04 16:46:35 -0300

    Atualizar o instalar_proxy_hunter.sh

--- h/instalar_proxy_hunter.sh
+++ h/instalar_proxy_hunter.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-# Alfa Proxy Hunter Installer 🚀 by AlfaBot
-
-echo "🔐 Configurando permissões de armazenamento..."
 termux-setup-storage
-
-echo "📁 Criando diretório para o script..."
 mkdir -p $HOME/alfa_tools
-
-echo "⬇️ Baixando Alfa Proxy Hunter..."
 curl -L -o $HOME/alfa_tools/alfa_proxy_hunter.sh "https://raw.githubusercontent.com/sofrenoob/Gggggg/main/h/alfa_proxy_hunter.sh"
-
-echo "🔓 Dando permissão de execução..."
 chmod +x $HOME/alfa_tools/alfa_proxy_hunter.sh
-
-echo "🚀 Executando o script agora..."
 bash $HOME/alfa_tools/alfa_proxy_hunter.sh
