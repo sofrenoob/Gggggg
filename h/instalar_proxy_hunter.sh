e7006bc7e79180b3f02409f9dba3e04c07ac9ebe
sofrenoob <sofrenoob@gmail.com>
2025-05-04 16:50:52 -0300

    Atualizar o instalar_proxy_hunter.sh

--- h/instalar_proxy_hunter.sh
+++ h/instalar_proxy_hunter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 termux-setup-storage
-mkdir -p $HOME/alfa_tools
+mkdir -p $HOME/alfa_tools/tmp
 curl -L -o $HOME/alfa_tools/alfa_proxy_hunter.sh "https://raw.githubusercontent.com/sofrenoob/Gggggg/main/h/alfa_proxy_hunter.sh"
 chmod +x $HOME/alfa_tools/alfa_proxy_hunter.sh
 bash $HOME/alfa_tools/alfa_proxy_hunter.sh
