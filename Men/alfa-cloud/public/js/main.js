d5d15887f1e14638852a95bee37fee96a09221d4
sofrenoob <sofrenoob@gmail.com>
2025-05-09 01:19:18 -0300

    Create main.js

---
+++ Men/alfa-cloud/public/js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const manageUsersButton = document.getElementById("manage-users");
+  const manageProxiesButton = document.getElementById("manage-proxies");
+  const viewConnectionsButton = document.getElementById("view-connections");
+  const systemTerminalButton = document.getElementById("system-terminal");
+
+  manageUsersButton.addEventListener("click", () => {
+    alert("User management coming soon!");
+  });
+
+  manageProxiesButton.addEventListener("click", () => {
+    alert("Proxy management coming soon!");
+  });
+
+  viewConnectionsButton.addEventListener("click", () => {
+    alert("Connection view coming soon!");
+  });
+
+  systemTerminalButton.addEventListener("click", () => {
+    alert("System terminal coming soon!");
+  });
+});
