6d6fca123a5f0e9ba69dc1796b9a7344a4446170
sofrenoob <sofrenoob@gmail.com>
2025-09-02 21:30:38 -0300

    Atualizar o install.sh

--- h/install.sh
+++ h/install.sh
@@ -9,7 +9,7 @@ sudo apt install -y software-properties-common
 echo "==== Instalando PHP e extensões ===="
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update
-sudo apt install -y php8.1 php8.1-cli php8.1-fpm php8.1-xml php8.1-mbstring php8.1-curl php8.1-zip php8.1-mysql php8.1-gd php8.1-bcmath php8.1-intl php8.1-json php8.1-pgsql unzip
+sudo apt install -y php8.1 php8.1-cli php8.1-fpm php8.1-xml php8.1-mbstring php8.1-curl php8.1-zip php8.1-mysql php8.1-gd php8.1-bcmath php8.1-intl php8.1-pgsql unzip
 
 echo "==== Instalando Composer ===="
 curl -sS https://getcomposer.org/installer | php
@@ -57,7 +57,7 @@ npm run build
 
 echo "==== Ajustando permissões ===="
 sudo chown -R www-data:www-data storage bootstrap/cache
-sudo chmod -R 775 storage bootstrap/cache
+sudo chmod -R 775 storage/bootstrap/cache
 sudo chown -R www-data:www-data public
 
 echo "==== Criando configuração do Nginx ===="
