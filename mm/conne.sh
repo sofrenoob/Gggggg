fec521d877880ac890afb7bd2425e34ae0804a35
sofrenoob <sofrenoob@gmail.com>
2025-10-11 19:59:53 -0300

    Atualizar o conne.sh

--- mm/conne.sh
+++ mm/conne.sh
@@ -1,63 +1,44 @@
 #!/bin/bash
-
-
-tput setaf 7 ; tput setab 4 ; tput bold ; printf '%50s%s%-20s\n' "BadVPN, created By Mr.Devim" ; tput sgr0
-if [ -f "/usr/local/bin/badvpn-udpgw" ]
-then
-	tput setaf 3 ; tput bold ; echo ""
-	echo ""
-	echo "O BadVPN já foi instalado com sucesso."
-	echo "Para executar, crie uma sessão screen"
-	echo "E execute o comando:"
-	echo ""
-	echo "badudp"
-	echo ""
-	echo "E deixe a sessão screen rodando em segundo plano."
-	echo "" ; tput sgr0
-	exit
-else
-tput setaf 2 ; tput bold ; echo ""
-echo -e "\033[1;36mEste é um script que compila e instala automaticamente o programa BadVPN em servidores Debian e Ubuntu para ativar o encaminhamento UDP na porta 7300, usado por programas como HTTP Injector da Evozi. Permitindo assim a utilização do protocolo UDP para jogos online, chamadas VoIP e outras coisas interessantes.\033[0m"
-echo "" ; tput sgr0
-read -p "Deseja continuar? [s/n]: " -e -i n resposta
-if [[ "$resposta" = 's' ]]; then
-	echo ""
-	echo -e "\033[1;31mA instalação pode demorar bastante... seja paciente!\033[0m"
-	sleep 3
-	apt-get update -y
-	apt-get install screen wget gcc build-essential g++ make -y
-	wget http://www.cmake.org/files/v2.8/cmake-2.8.12.tar.gz
-	tar xvzf cmake*.tar.gz
-	cd cmake*
-	./bootstrap --prefix=/usr
-	make 
-	make install
-	cd ..
-	rm -r cmake*
-	mkdir badvpn-build
-	cd badvpn-build
-	wget https://github.com/ambrop72/badvpn/archive/refs/tags/1.999.130.tar.gz
-	tar xf 1.999.130.tar.gz
-	cd bad*
-	cmake -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1
-	make install
-	cd ..
-	rm -r bad*
-	cd ..
-	rm -r badvpn-build
-    chmod +x badvpn.sh
-    ./badvpn.sh
-	echo "#!/bin/bash
-	badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 8" > /bin/badudp
-	chmod +x /bin/badudp
-	clear
-	tput setaf 3 ; tput bold ; echo ""
-	echo ""
-	echo -e "\033[1;36mBadVPN instalado com sucesso. Para usar, crie uma sessão screen e execute o comando badudp e deixe a sessão screen rodando em segundo plano.\033[0m"
-	echo "" ; tput sgr0
-	exit
-else 
-	echo ""
-	exit
-fi
-fi
+    clear
+    echo -e "\033[1;37m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "\E[44;1;37m            GERENCIAR BADVPN             \E[0m"
+echo -e "\033[1;37m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo ""
+    if ps x | grep -w udpvpn | grep -v grep 1>/dev/null 2>/dev/null; then
+        echo -e "\033[1;37mPORTAS\033[1;37m: \033[1;32m$(netstat -nplt | grep 'badvpn-ud' | awk {'print $4'} | cut -d: -f2 | xargs)"
+    else
+        sleep 0.1
+    fi
+    var_sks1=$(ps x | grep "udpvpn"|grep -v grep > /dev/null && echo -e "\033[1;32m◉ " || echo -e "\033[1;31m○ ")
+    echo ""
+			tput civis
+			echo -ne "\033[1;31m╰━━━━━━━━❪\033[1;32mESCOLHA OPÇÃO DESEJADA\033[1;33m\033[1;31m\033[1;37m : ";
+			read x
+			tput cnorm
+			clear
+			case $x in
+			1 | 01)
+			badvpn
+			;;
+			2 | 02)
+			badvpn2
+			;;
+			3 | 03)
+			badpro1
+			;;
+			4 | 04)
+			badvpn3
+			;;
+			0 | 00)
+			clear
+			menu
+			;;
+			*)
+			echo -e "\033[1;31mOpcao invalida !\033[0m"
+			sleep 2
+			;;
+			esac
+		done
+	}
+	fun_conexao
+}
