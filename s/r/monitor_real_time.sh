075b5ecc5e3017069c2db3331d9121265f876af7
sofrenoob <sofrenoob@gmail.com>
2025-10-17 19:18:08 -0300

    Delete s directory

--- s/r/monitor_real_time.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Monitor AlfaMenager em tempo real
-# by @alfalemos 👾🥷
-
-LOG_FILE="logs/conexoes.log"
-
-screen -dmS monitor tail -f $LOG_FILE
-echo -e "\e[1;32m[+] Monitor de conexões ativo via screen. Use 'screen -r monitor' pra acompanhar.\e[0m"
