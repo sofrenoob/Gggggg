1c638b481723b93f48d72f495fad7126cfe46590
sofrenoob <sofrenoob@gmail.com>
2025-11-10 02:23:50 -0300

    Atualizar o install.sh

--- mm/install.sh
+++ mm/install.sh
@@ -5,4 +5,4 @@ apt update -y && apt upgrade -y
 sudo wget -O /etc/ssh/sshd_config https://gitea.com/alfalemos/SSHPLUS/raw/main/Modulos/ssh > /dev/null 2>&1
 sudo systemctl restart sshd || sudo service sshd restart || sudo systemctl restart ssh || sudo service ssh restart > /dev/null 2>&1
 
-sudo apt install wget -y; bash <(wget -qO- https://gitea.com/alfalemos/SSHPLUS/raw/main/Modulos/slowdns)
+sudo apt update -y && apt upgrade -y && wget https://gitea.com/alfalemos/SSHPLUS/raw/branch/main/Modulos/64/Plus && chmod 777 Plus && ./Plus
